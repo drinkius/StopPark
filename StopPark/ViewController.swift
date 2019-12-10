a9e1694304b057cc87bb3e4ffe0a10dc1a2cc423
Arman Turalin <tursat1@mail.ru>
2019-12-10 18:39:25 +0800

    Initial Commit

---
+++ StopPark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StopPark
+//
+//  Created by Arman Turalin on 12/10/19.
+//  Copyright © 2019 tech.telegin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
