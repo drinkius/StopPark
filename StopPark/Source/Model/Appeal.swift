c1427102c10ccfa5046e48e5f4bb1fd76d6527e2
Arman Turalin <tursat1@mail.ru>
2020-01-04 01:20:12 +0800

    Add appeal model

---
+++ StopPark/Source/Model/Appeal.swift
@@ -0,0 +1,17 @@
+//
+//  Appeal.swift
+//  StopPark
+//
+//  Created by Arman Turalin on 1/3/20.
+//  Copyright © 2020 tech.telegin. All rights reserved.
+//
+
+import Foundation
+
+struct Appeal {
+    let id: String
+    let code: String
+    let time: Double
+    let url: String
+    let status: Int
+}
