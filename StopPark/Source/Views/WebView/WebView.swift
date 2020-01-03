3b8fd33a1f3588b2d5c60fd255a7395e5a9d4ee2
Arman Turalin <tursat1@mail.ru>
2020-01-03 14:47:19 +0800

    Refactor WebView for more readable

--- StopPark/Source/Views/WebView/WebView.swift
+++ StopPark/Source/Views/WebView/WebView.swift
@@ -13,15 +13,15 @@ protocol WebViewDelegate: class {
     func loading()
     func done()
     func showCapture(with url: URL?)
+    func showError(_ text: String?)
 }
 
 class WebView: BaseView {
         
     public weak var delegate: WebViewDelegate?
     
-    private var step: Int = 0
-    
-    private let url = URL(string: "https://xn--90adear.xn--p1ai/request_main")
+    private var isSessionUpdated: Bool = false
+    private var eventInfoForm: [FormData: String]?
     private let config = WKWebViewConfiguration()
     private lazy var web: WKWebView = {
         let w = WKWebView(frame: .zero, configuration: config)
@@ -33,19 +33,22 @@ class WebView: BaseView {
 
         
     override func setupView() {
-        loadRequest()
         configureViews()
         configureConstraints()
     }
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if keyPath == "estimatedProgress" {
-            
+            checkCookie()
             print("web progress: \(web.estimatedProgress)")
             if web.estimatedProgress >= 0.9 {
                 delegate?.done()
             }
         }
+    }
+    
+    deinit {
+        web.removeObserver(self, forKeyPath: "estimatedProgress")
     }
 }
 
@@ -64,42 +67,47 @@ extension WebView {
          web.bottomAnchor.constraint(equalTo: bottomAnchor)
             ].forEach { $0.isActive = true }
     }
+    
+    private func checkCookie() {
+        guard isSessionUpdated == false else { return }
+        guard let data = eventInfoForm else { return }
+        self.web.getCookies(completion: { dict in
+            let session = UserDefaultsManager.getSession()
+            guard !dict.isEmpty else {
+                guard session == nil else { return }
+                self.loadRequest(with: data)
+                print("should return")
+                return
+            }
+            
+            guard let sessionDict = dict["session"] as? [String: Any],
+                let newSession = sessionDict["Value"] as? String else {
+                self.loadRequest(with: data)
+                print("should return")
+                return
+            }
+            print("entered")
+            UserDefaultsManager.setSession(newSession)
+            self.isSessionUpdated = true
+
+            print("log cockie: \(dict)")
+        })
+    }
 }
 
 // MARK: - Actions
 extension WebView {
-    public func loadRequest() {
-        guard let urlRequest = RequestManager.shared.initialRequest() else {
+    public func loadRequest(with data: [FormData: String]) {
+        guard let urlRequest = RequestManager.shared.initialRequest(with: data) else {
             return
         }
         
+        eventInfoForm = data
+        
         web.load(urlRequest)
         delegate?.loading()
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0, execute: {
-            self.web.getCookies(completion: { dat in
-                guard !dat.isEmpty else {
-                    self.loadRequest()
-                    print("should return")
-                    return
-                }
-                
-                guard let sputnik_session = dat["session"] as? [String: Any], let ses = sputnik_session["Value"] as? String else {
-                    self.loadRequest()
-                    print("should return")
-                    return
-                }
-                print("entered")
-                UserDefaultsManager.setUploadImagesSession(ses)
-
-                print("log cockie: \(dat)")
-            })
-            DispatchQueue.main.asyncAfter(deadline: .now() + 10.0, execute: {
-                self.sendImageToServer(image: phot)
-            })
-        })
     }
-    
+        
     public func loadAppealRequest() {
         guard let urlRequest = RequestManager.shared.finalRequest() else {
             return
@@ -109,54 +117,19 @@ extension WebView {
         delegate?.loading()
     }
     
-    
-    private func openForm() {
-        web.evaluateJavaScript(Scripts.setCheckbox)
-        web.evaluateJavaScript(Scripts.submitForm)
-        delegate?.loading()
-    }
-    
-    private func fillForm() {
-        web.evaluateJavaScript("document.querySelector('#request > div.b-form > div:nth-child(1) > div.bf-item-holder > table > tbody > tr:nth-child(3) > td:nth-child(2) > input').value = 'Hello Result';")
-        print("form filled")
-        
-        web.evaluateJavaScript("document.querySelector('#request > div.b-form > div:nth-child(1) > div.bf-item-holder > table > tbody > tr:nth-child(1) > td:nth-child(2) > span > span.selection > span').value = '23 Краснодарский край';")
-    }
-    
-    
-    private func sendImageToServer(image: UIImage) {
+    public func sendImageToServer(image: UIImage, completion: @escaping (Result) -> ()) {
         guard let urlRequest = RequestManager.shared.uploadDataRequest(image: image) else {
+            completion(.failure("Ссылка неверная, напишите в поддержку."))
             return
         }
         
-        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
-            if let data = data {
-                if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                    if let file = json["file"] as? [String: Any] {
-                        if let id = file["id"] as? String, let sessionId = file["session_id"] as? String {
-                            print(json)
-                            UserDefaultsManager.setUploadImagesIds([id])
-//                            UserDefaultsManager.setUploadImagesSession(sessionId)
-                            print("id: \(id)")
-                        }
-                    }
-                }
-            }
-            
-            if let error = error {
-                print(error.localizedDescription)
-            }
-            
-            if let response = response as? HTTPURLResponse {
-                print(response.statusCode)
-            }
-            
-        }.resume()
+        NetworkManager.shared.uploadImage(to: urlRequest) { result in
+            completion(result)
+        }
     }
-
 }
 
-
+// MARK: - WKNavigationDelegate
 extension WebView: WKNavigationDelegate {
     func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
         if let response = navigationResponse.response as? HTTPURLResponse {
@@ -166,42 +139,15 @@ extension WebView: WKNavigationDelegate {
     }
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-//        web.evaluateJavaScript("document.querySelector('body > div.ln-page > div > div.ln-content.wrapper.clearfix > div:nth-child(4) > div > script:nth-child(1)').innerHTML", completionHandler: { responce, error in
-//            if let resp = responce as? String {
-//                print(resp)
-//                let range = NSRange(location: 0, length: resp.utf16.count)
-//                let regex = try! NSRegularExpression(pattern: "[^step.+](\\d)")
-//                let result = regex.firstMatch(in: resp, options: [], range: range)
-//                if let nsrange = result?.range(at: 1) {
-//                    let range = Range(nsrange, in: resp)
-//
-//                    print("result \(resp[range!])")
-//                    self.step = Int(String(resp[range!]))!
-//                }
-//            }
-//            switch self.step {
-//            case 0, 1: self.openForm()
-//            case 2: self.fillForm()
-//            default: break
-//            }
-//        })
         web.evaluateJavaScript(Scripts.getCaptureImageURL, completionHandler: { resp, error in
             if let urlString = resp as? String, let url = URL(string: urlString) {
                 self.delegate?.showCapture(with: url)
             }
-            
         })
     }
 }
 
-extension WebView: WKScriptMessageHandler {
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-
-        guard let dict = message.body as? [String: Any] else { return }
-        print(message.body)
-    }
-}
-
+// MARK: - Support
 extension WebView {
     enum Scripts {
         static let submitForm = "$('.u-form__sbt').click();"
