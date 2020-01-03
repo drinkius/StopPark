496abe4133d2a9185e3cc1f810246419fa6b8151
Arman Turalin <tursat1@mail.ru>
2020-01-03 14:32:38 +0800

    Add NetworkManager

---
+++ StopPark/Resource/NetworkManager.swift
@@ -0,0 +1,40 @@
+//
+//  NetworkManager.swift
+//  StopPark
+//
+//  Created by Arman Turalin on 12/31/19.
+//  Copyright © 2019 tech.telegin. All rights reserved.
+//
+
+import Foundation
+
+enum Result {
+    case failure(String)
+    case success
+}
+
+class NetworkManager {
+    static let shared = NetworkManager()
+        
+    func uploadImage(to url: URLRequest, completion: @escaping (Result) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                let description = error?.localizedDescription ?? "Нет соединения."
+                completion(.failure(description))
+                return
+            }
+            
+            guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                completion(.failure("Не смогли получить данные."))
+                return
+            }
+            guard let file = json["file"] as? [String: Any], let id = file["id"] as? String else {
+                completion(.failure("Не смогли получить данные."))
+                return
+            }
+            
+            print(json)
+            UserDefaultsManager.setUploadImagesIds([id])
+        }.resume()
+    }
+}
